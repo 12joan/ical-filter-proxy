c0dbcad2842db03522bf7e85eadf39b32e6c99cc
Joe Anderson <joe@osbert.me>
2021-10-04 13:02:23 +0100

    Remove hardcoded format option for rspec
    Override using the `--format documentation` command line option.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,6 @@ require 'ical_filter_proxy'
 RSpec.configure do |config|
   # Use color in STDOUT
   config.color = true
-
-  # Use the specified formatter
-  config.formatter = :documentation
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
